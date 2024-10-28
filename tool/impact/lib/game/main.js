3395bc16bbdc850fde7dd9298f18fde6ed911dca
Richard Perez <richardp168@nycstudents.net>
2024-10-28 11:52:02 -0400

    pushing

--- tool/impact/lib/game/main.js
+++ tool/impact/lib/game/main.js
@@ -1,5 +1,5 @@
-ig.module( 
-	'game.main' 
+ig.module(
+	'game.main'
 )
 .requires(
 	'impact.game',
@@ -8,31 +8,35 @@ ig.module(
 .defines(function(){
 
 MyGame = ig.Game.extend({
-	
+
 	// Load a font
 	font: new ig.Font( 'media/04b03.font.png' ),
-	
-	
+
+
 	init: function() {
+		ig.input.bind( ig.KEY.LEFT_ARROW, 'left');
+		ig.input.bind( ig.KEY.DOWN_ARROW, 'down');
+		ig.input.bind( ig.KEY.UP_ARROW, 'up');
+		ig.input.bind( ig.KEY.RIGHT_ARROW, 'right');
 		// Initialize your game here; bind keys etc.
 	},
-	
+
 	update: function() {
 		// Update all entities and backgroundMaps
 		this.parent();
-		
+
 		// Add your own, additional update code here
 	},
-	
+
 	draw: function() {
 		// Draw all entities and backgroundMaps
 		this.parent();
-		
-		
+
+
 		// Add your own drawing code here
 		var x = ig.system.width/2,
 			y = ig.system.height/2;
-		
+
 		this.font.draw( 'It Works!', x, y, ig.Font.ALIGN.CENTER );
 	}
 });
